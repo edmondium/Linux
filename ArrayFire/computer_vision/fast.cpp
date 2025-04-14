5925d67e97c6d7ae4231dc5c2cef9d5f2a22af9e
edmondium <nicko@uin-suska.ac.id>
2025-04-14 07:01:10 +0200

    Features from Accelerated Segment Test

--- ArrayFire/computer_vision/fast.cpp
+++ ArrayFire/computer_vision/fast.cpp
@@ -1,40 +1,47 @@
+/*******************************************************
+ * Copyright (c) 2015, ArrayFire
+ * All rights reserved.
+ *
+ * This file is distributed under 3-clause BSD license.
+ * The complete license agreement can be obtained at:
+ * http://arrayfire.com/licenses/BSD-3-Clause
+ ********************************************************/
+
 #include <arrayfire.h>
-#include <iostream>
+#include <cstdio>
 #include <cstdlib>
 
 using namespace af;
 
-void fast_demo(bool console) {
-    // Load image based on console mode
-    array img_color = console 
-        ? loadImage("../images/square.png", true)
-        : loadImage("../images/man.jpg", true);
-
-    // Convert image from RGB to grayscale
+static void fast_demo(bool console) {
+    // Load image
+    array img_color;
+    if (console)
+        img_color = loadImage("../images/square.png", true);
+    else
+        img_color = loadImage("../images/man.jpg", true);
+    // Convert the image from RGB to gray-scale
     array img = colorSpace(img_color, AF_GRAY, AF_RGB);
-
-    // Normalize color image for visualization
+    // For visualization in ArrayFire, color images must be in the [0.0f-1.0f]
+    // interval
     img_color /= 255.f;
 
-    // Perform FAST feature detection
-    auto feat = fast(img, 20.0f, 9, true, 0.05);
+    features feat = fast(img, 20.0f, 9, true, 0.05);
 
-    // Retrieve feature coordinates
-    auto h_x = feat.getX().host<float>();
-    auto h_y = feat.getY().host<float>();
+    float* h_x = feat.getX().host<float>();
+    float* h_y = feat.getY().host<float>();
 
-    // Draw crosshairs at detected features
-    constexpr int draw_len = 3;
+    // Draw draw_len x draw_len crosshairs where the corners are
+    const int draw_len = 3;
     for (size_t f = 0; f < feat.getNumFeatures(); f++) {
-        int x = h_x[f];
-        int y = h_y[f];
-
-        // Draw horizontal line
+        int x                                            = h_x[f];
+        int y                                            = h_y[f];
         img_color(y, seq(x - draw_len, x + draw_len), 0) = 0.f;
         img_color(y, seq(x - draw_len, x + draw_len), 1) = 1.f;
         img_color(y, seq(x - draw_len, x + draw_len), 2) = 0.f;
 
-        // Draw vertical line
+        // Draw vertical line of (draw_len * 2 + 1) pixels centered on  the
+        // corner Set only the first channel to 1 (green lines)
         img_color(seq(y - draw_len, y + draw_len), x, 0) = 0.f;
         img_color(seq(y - draw_len, y + draw_len), x, 1) = 1.f;
         img_color(seq(y - draw_len, y + draw_len), x, 2) = 0.f;
@@ -43,12 +50,12 @@ void fast_demo(bool console) {
     freeHost(h_x);
     freeHost(h_y);
 
-    // Print the number of features found
-    std::cout << "Features found: " << feat.getNumFeatures() << '\n';
+    printf("Features found: %zu\n", feat.getNumFeatures());
 
-    // Display or print the results
     if (!console) {
-        Window wnd("FAST Feature Detector");
+        af::Window wnd("FAST Feature Detector");
+
+        // Previews color image with green crosshairs
         while (!wnd.close()) wnd.image(img_color);
     } else {
         af_print(feat.getX());
@@ -57,21 +64,19 @@ void fast_demo(bool console) {
 }
 
 int main(int argc, char** argv) {
-    // Parse command-line arguments
-    int device = argc > 1 ? std::stoi(argv[1]) : 0;
-    bool console = argc > 2 && argv[2][0] == '-';
+    int device   = argc > 1 ? atoi(argv[1]) : 0;
+    bool console = argc > 2 ? argv[2][0] == '-' : false;
 
     try {
-        // Set device and display ArrayFire info
         af::setDevice(device);
         af::info();
-
-        std::cout << "** ArrayFire FAST Feature Detector Demo **\n\n";
+        printf("** ArrayFire FAST Feature Detector Demo **\n\n");
         fast_demo(console);
-    } catch (const af::exception& ae) {
-        // Handle exceptions and print error
-        std::cerr << ae.what() << '\n';
+
+    } catch (af::exception& ae) {
+        fprintf(stderr, "%s\n", ae.what());
         throw;
     }
+
     return 0;
 }
