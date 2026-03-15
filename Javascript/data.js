d9d3f9d7f870e8765f3e16a2cc22e245c370db05
edmondium <nicko@uin-suska.ac.id>
2026-03-15 16:31:55 +0700

    Learning data in javascript

---
+++ Javascript/data.js
@@ -0,0 +1,23 @@
+const employees = [
+  {
+    name: 'Fulan',
+    email: 'fulan@dicoding.com',
+    joinYear: 2020,
+  },
+];
+
+function addEmployee(name, email, joinYear) {
+  /**
+   * @TODO
+   * lengkapi fungsi ini agar dapat menambahkan objek employee baru
+   * berdasarkan nilai argumen fungsi dan simpan ke dalam array `employees`
+   */
+   const newEmployee = {
+     name : name,
+     email : email,
+     joinYear : joinYear,
+   };
+  employees.push(newEmployee);
+}
+addEmployee("Budi", "budi@dicoding.com", 2026);
+console.log(employees)
