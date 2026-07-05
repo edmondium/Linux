0cfb9f8bbe91a971af3e6ccae539a70fd38ccde5
edmondium <nicko@uin-suska.ac.id>
2026-07-05 21:27:39 +0700

    training test

---
+++ R/DS Dasar Dataset/practice2.R
@@ -0,0 +1,11 @@
+df <- read.csv("Social_Network_Ads.csv")
+df$User.ID <- NULL
+library(caTools)
+set.seed(123)
+split <- sample.split(df$Purchased, SplitRatio = 0.7)
+training_set <- subset(df, split == TRUE)
+test_set <- subset(df, split == FALSE)
+training_set$Purchased <- as.factor(training_set$Purchased)
+test_set$Purchased <- as.factor(test_set$Purchased)
+summary(training_set$Purchased)
+summary(test_set$Purchased)
