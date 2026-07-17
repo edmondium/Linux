3e3b3a9e085a2639177d91efe5007cc685318bba
edmondium <nicko@uin-suska.ac.id>
2026-07-17 19:42:59 +0700

    k-means clustering

--- R/DS Dasar Dataset/kMeans.R
+++ R/DS Dasar Dataset/kMeans.R
@@ -7,3 +7,10 @@ plot(1:10, wcsss, type = "b",
      main = paste("cluster of pelanggan"),
      xlab = "number",
      ylab = wcsss)
+set.seed(29)
+kmeans6 <- kmeans(data, 6, iter.max = 10, nstart = 10)
+kmeans6$size
+kmeans6$cluster
+kmeans6$centers
+kmeans4 <- kmeans(data, 4, iter.max = 10, nstart = 10)
+kmeans4$size
