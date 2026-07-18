3d60f04343d10791bf2bb06ea2108275b424d3c7
edmondium <nicko@uin-suska.ac.id>
2026-07-18 20:55:12 +0700

    time series

---
+++ R/DS Dasar Dataset/timeSeries.R
@@ -0,0 +1,11 @@
+data <- read.csv("Timeseries Data.csv")
+X <- data[, 2]
+library(forecast)
+X_TS <- ts(X, frequency = 12, start = c(2014, 1))
+plot(X_TS)
+mod <- auto.arima(X_TS)
+result <- data.frame(mod$fitted)
+X_predict <- forecast(mod, h = 13)
+accuracy(X_predict)
+plot(X_predict)
+X_predict <- data.frame(X_predict)
